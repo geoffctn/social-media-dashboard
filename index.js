d5317f359cb5b05b782bbd1400ea39816204ffb8
Geoffrey <geoffrey.couten@gmail.com>
2020-10-01 15:41:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// script for dark mode handling
+const checkbox = document.getElementById('switch')
+const body = document.getElementsByTagName('body')[0]
+
+checkbox.addEventListener('change', () => {
+  if (checkbox.checked) {
+    body.classList.add('dark-mode')
+  } else {
+    body.classList.remove('dark-mode')
+  }
+})
